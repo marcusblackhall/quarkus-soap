2e01760e3496b3fc1930e79e4593020c5e5ed81a
Marcus Blackhall <marcusinamsterdam@gmail.com>
2024-11-02 21:20:14 +0100

    Add soap request test

---
+++ src/test/java/nl/ilovecoding/lookatsoap/HelloSoapTest.java
@@ -0,0 +1,43 @@
+package nl.ilovecoding.lookatsoap;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Collectors;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+@QuarkusTest
+public class HelloSoapTest {
+
+    @Test
+    void shouldRetrieveCity() throws IOException {
+
+         given()
+                .auth()
+                .preemptive()
+                .basic("marcus","marcus12")
+                .log().all()
+                .body(bodyfFromFile())
+                .when().post("/soap/sayhello")
+                .then()
+                .log().body()
+                .assertThat()
+                .body(hasXPath("//return"),containsString("Hello Marcus"))
+                .statusCode(200).extract().body().asString();
+
+    }
+
+    private String  bodyfFromFile() throws IOException {
+
+        Path path = Path.of("src","test","resources","request.xml");
+        String collect = Files.lines(path).collect(Collectors.joining());
+        return collect;
+
+
+    }
+}
